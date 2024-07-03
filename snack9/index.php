b553f4c1c1890329f9549f8f81837f35fde3f210
Piccios <lorenzo.hl.picchi@gmail.com>
2024-07-03 12:31:50 +0200

    test set on index.php

--- snack9/index.php
+++ snack9/index.php
@@ -3,3 +3,16 @@
 require_once __DIR__ . "/src/classes/User.php";
 require_once __DIR__ . "/src/classes/PremiumUser.php";
 require_once __DIR__ . "/src/classes/Membership.php";
+
+
+try {
+    $membership = new Membership("Gold", 99.99);
+    $premiumUser = new PremiumUser("Mario Rossi", "mario.rossi@example.com", $membership);
+
+    echo "Nome: " . $premiumUser->getName() . "\n";
+    echo "Email: " . $premiumUser->getEmail() . "\n";
+    echo "Livello Membership: " . $premiumUser->getMembership()->getLevel() . "\n";
+    echo "Prezzo Membership: " . $premiumUser->getMembership()->getPrice() . "\n";
+} catch (Exception $err) {
+    echo 'Errore: ' . $err->getMessage();
+}
